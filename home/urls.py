99dabaf0db3477722706c99434152fe778c82d62
Andrija <burlicconi@gmail.com>
2016-05-08 12:03:02 +0200

    updated urls, to avoid warning

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
 from . import views
 from django.conf.urls import patterns, include, url
  
-urlpatterns = patterns('home.views',
-               url(r'^login$',views.login_view, name='login'),
+urlpatterns = [url(r'^login$',views.login_view, name='login'),
                url(r'^login(?P<next>.+)',views.login_view, name='login'),
                url(r'^logout$', views.logout_view,name='logout'),
-               )
+               ]
