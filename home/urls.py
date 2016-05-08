896238d5e873ed1ea5dd9908a1bef403057afa22
Andrija <burlicconi@gmail.com>
2016-05-08 15:13:12 +0200

    test2

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [url(r'^login$',views.login_view, name='login'),
                url(r'^login(?P<next>.+)',views.login_view, name='login'),
                url(r'^logout$', views.logout_view,name='logout'),
                ]
-#test
+#test 222222
